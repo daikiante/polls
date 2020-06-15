f9ed3a279ac99f3983e329929ded3929ee319df4
daikiante <daikiante@gmail.com>
2020-06-15 15:35:53 +0900

    tutorial donegit add .

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 
@@ -118,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/css"),
+)
